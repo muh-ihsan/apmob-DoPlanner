e76f414fba2aefc2de9931bc84586464b2ccc7d3
muh-ihsan <muh_ihsan@outlook.com>
2021-05-30 21:09:39 +0700

    test komentar

--- app/src/main/java/com/tubesApmob/doplanner/ForgotPasswordFragment.kt
+++ app/src/main/java/com/tubesApmob/doplanner/ForgotPasswordFragment.kt
@@ -3,4 +3,5 @@ package com.tubesApmob.doplanner
 import androidx.fragment.app.Fragment
 
 class ForgotPasswordFragment : Fragment(R.layout.dpl_forgot_password_fragment) {
+    // tes
 }
--- app/src/main/java/com/tubesApmob/doplanner/LoginFragment.kt
+++ app/src/main/java/com/tubesApmob/doplanner/LoginFragment.kt
@@ -7,4 +7,5 @@ import android.view.View
 import android.view.ViewGroup
 
 class LoginFragment : Fragment(R.layout.dpl_login_fragment) {
+    // Ini untuk tes
 }
